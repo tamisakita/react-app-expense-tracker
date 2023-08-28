b6a3b304acd6a026be37ae3c342958c73d88e24e
Patricia Tami Sakita <tamisakita@hotmail.com>
2023-08-27 21:42:10 -0700

    add form

---
+++ src/components/Form.tsx
@@ -0,0 +1,76 @@
+import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  description: z
+    .string()
+    .min(3, { message: "Description should be at least 3 characters." }),
+  amount: z
+    .number({ invalid_type_error: "Amount is required." })
+    .min(1, { message: "Amount is required." }),
+});
+
+type FormData = z.infer<typeof schema>;
+
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="description" className="form-label">
+          Description
+        </label>
+        <input
+          {...register("description")}
+          id="description"
+          type="text"
+          className="form-control"
+        ></input>
+        {errors.description && (
+          <p className="text-danger">{errors.description.message}</p>
+        )}
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="amount" className="form-label">
+          Amount
+        </label>
+        <input
+          {...(register("amount"), { valueAsNumber: true })}
+          id="amount"
+          type="number"
+          className="form-control"
+        ></input>
+        {errors.amount && (
+          <p className="text-danger">{errors.amount.message}</p>
+        )}
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="category" className="form-label">
+          Category
+        </label>
+        <select className="form-select" id="category">
+          <option></option>
+          <option>Groceries</option>
+          <option>Utilities</option>
+          <option>Entertainment</option>
+        </select>
+      </div>
+
+      <button disabled={!isValid} className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
